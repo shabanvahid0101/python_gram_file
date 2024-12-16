d1da0418274498aab329c5b23861612e656c61cf
shabanvahid0101 <shabanvahid0101@gmail.com>
2024-12-16 21:24:53 +0900

    new file add

---
+++ kivy library/my_kivy_app.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.label import Label
+class mykivy(App):
+    def build(self):
+        return Label(text='Hello, World')
+if __name__ == "__main__":
+    mykivy().run()
